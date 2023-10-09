80c6cc99a4b5ef059051393a60c65d2edb3cfb50
codepapa <codepapa360@gmail.com>
2023-10-09 20:16:49 +0600

    implimented-local-storage-persistence

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,35 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const tempTodo = [
-  {
-    id: 10,
-    content: "Going to scool",
-    done: false,
-  },
-  {
-    id: 11,
-    content: "Playing football",
-    done: true,
-  },
-  {
-    id: 12,
-    content: "Talking to friends",
-    done: false,
-  },
-  {
-    id: 13,
-    content: "Food shopping",
-    done: false,
-  },
-  {
-    id: 14,
-    content: "Filling the gas to the car",
-    done: true,
-  },
-];
+// const tempTodo = [
+//   {
+//     id: 10,
+//     content: "Going to scool",
+//     done: false,
+//   },
+//   {
+//     id: 11,
+//     content: "Playing football",
+//     done: true,
+//   },
+//   {
+//     id: 12,
+//     content: "Talking to friends",
+//     done: false,
+//   },
+//   {
+//     id: 13,
+//     content: "Food shopping",
+//     done: false,
+//   },
+//   {
+//     id: 14,
+//     content: "Filling the gas to the car",
+//     done: true,
+//   },
+// ];
+
+// const initialState = {
+//   todos: tempTodo,
+//   filter: "all",
+// };
+
+const loadTodosFromLocalStorage = () => {
+  try {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  } catch (error) {
+    console.error("Error loading todos from local storage:", error);
+    return [];
+  }
+};
 
 const initialState = {
-  todos: tempTodo,
+  todos: loadTodosFromLocalStorage(),
   filter: "all",
 };
 
@@ -45,18 +60,22 @@ const todoSlice = createSlice({
       },
       reducer(state, action) {
         state.todos = [action.payload, ...state.todos];
+        localStorage.setItem("todos", JSON.stringify(state.todos));
       },
     },
     deleteTodo(state, action) {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     doneTodo(state, action) {
       const targetTodo = state.todos.find((todo) => todo.id === action.payload);
 
       targetTodo.done = !targetTodo.done;
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     clearCompletedTodos(state) {
       state.todos = state.todos.filter((todo) => todo.done !== true);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     filterAll(state) {
       state.filter = "all";
